dc53034f1f50d74403e541a9cdbc062c2a16f7b8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 02:47:43 +0530

    Update main.py

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -12,6 +12,12 @@ async def detect(
         image: UploadFile = File(...)
 ):
     # Base 64 decode the image
+    if image and image.content_type != "image/jpeg":
+        return {300: {"description": "Only jpeg images are supported"}}
+    else:
+        contents = await image.read()
+        nparray = np.fromstring(contents, np.uint8)
+        img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
 
     # Run the model on the image
 
