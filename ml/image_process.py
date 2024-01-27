871a5bc39eed83f1bcc2c38b4bee3499323195cb
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 09:14:10 +0530

    Update image_process.py

--- ml/image_process.py
+++ ml/image_process.py
@@ -12,24 +12,13 @@ base_model = VGG16(include_top = False, weights = 'imagenet', input_shape = (224
 # Load the model
 model = load_model('vgg16.h5')
 
-# Load the image
-img_path = 'test.jpg'
-img = load_img(img_path, target_size=(224, 224))
-x = img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+from keras.models import Model
 
-# Get the output of the last convolutional layer
-vgg16_model = model.get_layer('vgg16')
+# Get the output of the last layer of the base model
+last_layer = base_model.get_layer(index=-1).output
 
-# Get the output of the last convolutional layer
-last_conv_layer = base_model.get_layer('block5_conv3')
+# Create a new output layer that outputs both the final predictions and the last layer's output
+new_output_layer = [model.output, last_layer]
 
-# Create a new model that outputs both the final predictions and the last convolutional layer's output
-output_model = tf.keras.Model(inputs=base_model.input, outputs=[model.output, last_conv_layer.output])
-
-# Use this model to predict on your input
-preds, conv_output = output_model.predict(x)
-
-print(preds)
-print(conv_output)
+# Create a new model that uses the same input as the original model but outputs the new output layer
+new_model = Model(inputs=model.input, outputs=new_output_layer)
