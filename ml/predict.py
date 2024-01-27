e81cb97ff9c8f7ffa03a64c55de5a6e6e128aa08
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 18:25:05 +0530

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -1,19 +1,7 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
-import os
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-
-from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
-from tensorflow.keras.models import Model, Sequential
-
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.layers import GlobalAveragePooling2D
 
 loaded_model = tf.keras.models.load_model("model.h5")
 label_to_class_name = {0: 'Cyst', 1: 'Normal', 2: 'Stone', 3: 'Tumor'}
