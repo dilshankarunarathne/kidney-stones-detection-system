448419a50f364b1d1664db15cbba98c87436ba21
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 18:48:23 +0530

    Update Checker.js

--- frontend/src/pages/components/Checker.js
+++ frontend/src/pages/components/Checker.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
