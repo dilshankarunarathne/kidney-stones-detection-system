3addb776d22af092332b9b62423e9acbfb907c91
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 09:20:08 +0530

    Update image_process.py

--- ml/image_process.py
+++ ml/image_process.py
@@ -5,22 +5,87 @@ import numpy as np
 from keras.applications.vgg16 import preprocess_input
 import cv2
 import tensorflow as tf
-from keras.applications import VGG16
 
-base_model = VGG16(include_top = False, weights = 'imagenet', input_shape = (224,224,3), classes = 38)
+
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Load the model
 model = load_model('vgg16.h5')
 
-from keras.models import Model
+# Define the last conv layer
+last_conv_layer_name = "block5_conv3"
 
-from keras.models import Model
+# Load the image
+img_path = 'test.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = preprocess_input(img_array)
 
-# Get the output of the last layer of the base model
-last_layer = base_model.layers[-1].output
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
 
-# Create a new output layer that outputs both the final predictions and the last layer's output
-new_output_layer = [model.layers[-1].output, last_layer]
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
 
-# Create a new model that uses the same input as the base model but outputs the new output layer
-new_model = Model(inputs=base_model.input, outputs=new_output_layer)
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+# Make model predictions
+preds = model.predict(img_array)
+
+# Generate class activation heatmap
+heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+
+# Display heatmap
+plt.matshow(heatmap)
+plt.show()
+
+# We load the original image
+img = cv2.imread(img_path)
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+heatmap = np.uint8(255 * heatmap)
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+superimposed_img = heatmap * 0.4 + img
+cv2.imwrite('output.jpg', superimposed_img)
+
+# Display Original Image
+plt.imshow(img)
+plt.title('Original Image')
+plt.show()
+
+# Display Grad CAM
+img = cv2.imread('/path_to_save_superimposed_image/superimposed.jpg')
+plt.imshow(img)
+plt.title('Grad CAM')
+plt.show()
