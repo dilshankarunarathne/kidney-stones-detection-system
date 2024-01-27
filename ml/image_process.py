11815b5d6cb2d606bfdc255bcb05906b6cd2a579
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 08:52:13 +0530

    Create image_process.py

---
+++ ml/image_process.py
@@ -0,0 +1,47 @@
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+from tensorflow.keras import backend as K
+import numpy as np
+import cv2
+
+# Load the model
+model = load_model('vgg16.h5')
+
+# Load the image
+img_path = 'test.jpg'
+img = load_img(img_path, target_size=(224, 224))
+x = img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+# Get the output of the last convolutional layer
+vgg16_model = model.get_layer('vgg16')
+last_conv_layer = vgg16_model.get_layer('block5_conv3')
+
+# Get the class index
+preds = model.predict(x)
+class_idx = np.argmax(preds[0])
+
+# Get the weights of the last dense layer
+class_output = model.output[:, class_idx]
+grads = K.gradients(class_output, last_conv_layer.output)[0]
+pooled_grads = K.mean(grads, axis=(0, 1, 2))
+iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+pooled_grads_value, conv_layer_output_value = iterate([x])
+for i in range(512):
+    conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+
+# Compute the dot product
+heatmap = np.mean(conv_layer_output_value, axis=-1)
+
+# Resize the result to the size of the input image
+heatmap = np.maximum(heatmap, 0)
+heatmap /= np.max(heatmap)
+heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+
+# Overlay the result on the input image
+heatmap = np.uint8(255 * heatmap)
+heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+
+# Save the image
+cv2.imwrite('output.jpg', superimposed_img)
