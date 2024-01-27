096c95c6f4ea9dd77aeacd1da8f67ca973779a5c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 18:23:18 +0530

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -2,4 +2,10 @@
 
 loaded_model = tf.keras.models.load_model("model.h5")
 
-
+img = cv2.imread('test.jpg')
+plt.imshow(img)
+plt.show()
+resize = tf.image.resize(img, (150,150))
+yhat = loaded_model.predict(np.expand_dims(resize/255, 0))
+max_index = np.argmax(yhat)
+label_to_class_name[max_index]
