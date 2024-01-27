1222339d3bffc378f5856d943e0e629dad185d7f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 18:36:19 +0530

    Create CustomBarChart.js

---
+++ frontend/src/pages/components/CustomBarChart.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+export default function CustomBarChart() {
+  return (
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      height={300}
+    />
+  );
+}
