75487b50325e2ebe8210a78aafdecf4400f47484
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 18:23:40 +0530

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -1,4 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+import os
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
+from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.models import Model, Sequential
+
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D
 
 loaded_model = tf.keras.models.load_model("model.h5")
 
