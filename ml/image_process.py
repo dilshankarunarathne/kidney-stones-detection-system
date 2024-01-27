f99bdb6740e357492634eb1f617e352f8f7da6dd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-27 09:03:39 +0530

    Update image_process.py

--- ml/image_process.py
+++ ml/image_process.py
@@ -1,48 +1,44 @@
+# Python
 from keras.models import load_model
-from keras.utils import load_img, img_to_array
-from tensorflow.keras import backend as K
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
 import numpy as np
 import cv2
 import tensorflow as tf
-
 
 # Load the model
 model = load_model('vgg16.h5')
 
 # Load the image
 img_path = 'test.jpg'
-img = load_img(img_path, target_size=(224, 224))
-x = img_to_array(img)
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
 # Get the output of the last convolutional layer
 vgg16_model = model.get_layer('vgg16')
 last_conv_layer = vgg16_model.get_layer('block5_conv3')
 
-# Create a new model that outputs the last convolutional layer's output
-output_model = tf.keras.Model(inputs=model.input, outputs=last_conv_layer.output)
+# Create a new model that outputs both the final predictions and the last convolutional layer's output
+output_model = tf.keras.Model(inputs=model.input, outputs=[model.output, last_conv_layer.output])
 
 # Use this model to predict on your input
-last_conv_layer_output = output_model.predict(x)
+preds, conv_output = output_model.predict(x)
+
+# Get the class index
+class_idx = np.argmax(preds[0])
 
 with tf.GradientTape() as tape:
-    # Get the class index
-    preds = model.predict(x)
-    class_idx = np.argmax(preds[0])
-
-    # Get the weights of the last dense layer
-    class_output = model.output[:, class_idx]
-
-    tape.watch(last_conv_layer_output)
+    tape.watch(conv_output)
     class_output = model.output[:, class_idx]
 
 # Get the gradient and the output of the last convolutional layer
-grads = tape.gradient(class_output, last_conv_layer_output)
-conv_layer_output_value = tf.keras.backend.get_value(last_conv_layer_output)
+grads = tape.gradient(class_output, conv_output)
 pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
 # Compute the dot product
-heatmap = np.mean(conv_layer_output_value, axis=-1)
+heatmap = np.mean(conv_output, axis=-1)
 
 # Resize the result to the size of the input image
 heatmap = np.maximum(heatmap, 0)
